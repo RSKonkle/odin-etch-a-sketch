f00f1b14a17628769d6838306cd7a787c24f82c9
Reagan Konkle <regosmallz@gmail.com>
2024-08-27 14:18:50 -0400

    Add function createDivs to create 16x16 grid using DOM manipulation

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // JavaScript - Etch-a-Sketch //
+const container = document.querySelector(".container");
+
+// OBJECTIVE - develop function to create 16x16 grid of square divs
+// createDivs: loop that will create divs until specified number is reached
+function createDivs(num = 256) {
+    for (let i = 1; i <= num; i++) {
+        const tile = document.createElement("div");
+        tile.classList.add("tile");
+        container.appendChild(tile);
+    }
+}
+
+createDivs();
